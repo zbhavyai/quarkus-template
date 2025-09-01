a172e56e134ba0f176455a09967fa1c0f3e4aefa
Bhavyai Gupta <zbhavyai@gmail.com>
2025-08-31 19:26:57 -0600

    update ping endpoint test

--- src/test/java/io/github/zbhavyai/quarkustemplate/rest/ping/PingResourceTest.java
+++ src/test/java/io/github/zbhavyai/quarkustemplate/rest/ping/PingResourceTest.java
@@ -4,6 +4,7 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 import io.quarkus.test.junit.QuarkusTest;
+import jakarta.ws.rs.core.MediaType;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
@@ -11,6 +12,12 @@ public class PingResourceTest {
 
   @Test
   void testPingEndpoint() {
-    given().when().get("/ping").then().statusCode(200).body(is("pong\n"));
+    given()
+        .when()
+        .get("/ping")
+        .then()
+        .statusCode(200)
+        .contentType(MediaType.TEXT_PLAIN)
+        .body(is("pong\n"));
   }
 }
