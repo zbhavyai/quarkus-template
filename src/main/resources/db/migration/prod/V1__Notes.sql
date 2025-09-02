77590213cb64b2c045c877ce37f9323479168c1f
Bhavyai Gupta <zbhavyai@gmail.com>
2025-09-01 20:23:59 -0600

    add flyway migration for notes and some dummy data

---
+++ src/main/resources/db/migration/prod/V1__Notes.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "quarkustemplate"."notes" (
+    "id" VARCHAR(255) NOT NULL,
+    "optlock" BIGINT NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "content" TEXT,
+    "created_at" TIMESTAMP(6) WITH TIME ZONE NOT NULL,
+    "updated_at" TIMESTAMP(6) WITH TIME ZONE NOT NULL,
+    PRIMARY KEY ("id")
+)
