a6b9f0940db70220285b567c6b8d38a2df76c483
Bhavyai Gupta <zbhavyai@gmail.com>
2025-09-15 22:28:47 -0600

    remove integration tests

--- src/test/java/io/github/zbhavyai/quarkustemplate/api/rest/note/NoteResourceIT.java
+++
@@ -1,6 +0,0 @@
-package io.github.zbhavyai.quarkustemplate.api.rest.note;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class NoteResourceIT extends NoteResourceTest {}
--- src/test/java/io/github/zbhavyai/quarkustemplate/api/rest/ping/PingResourceIT.java
+++
@@ -1,6 +0,0 @@
-package io.github.zbhavyai.quarkustemplate.api.rest.ping;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class PingResourceIT extends PingResourceTest {}
